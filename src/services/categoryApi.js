ff77703da4387fcf141e19337bf0e1c7e6585dd5
eetu <eetuhuttula99@gmail.com>
2026-01-21 10:56:13 +0200

    feat: Update API URLs to use environment variables for better configuration

--- src/services/categoryApi.js
+++ src/services/categoryApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3001/api';
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const getCategories = async () => {
   const response = await axios.get(`${API_URL}/categories`);
--- src/services/productApi.js
+++ src/services/productApi.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3001/api';
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const getProducts = async () => {
   try {
